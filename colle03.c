0fb14e595e531ca2fdb2bd9aaff14e6703274b72
Mathieu PAUL <mpaul@student.42.fr>
2016-09-04 18:07:01 +0200

    rendu final

--- colle03.c
+++ colle03.c
@@ -6,59 +6,57 @@
 /*   By: mpaul <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/03 18:41:20 by mpaul             #+#    #+#             */
-/*   Updated: 2016/09/04 05:04:43 by mpaul            ###   ########.fr       */
+/*   Updated: 2016/09/04 17:31:09 by mpaul            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_putchar(char c);
 
-void	colle03(int x, int y)
+void	ft_putb(int x, char c)
 {
-	int i;
-	int j;
+	while ((x - 2) > 0)
+	{
+		ft_putchar(c);
+		x--;
+	}
+}
 
-	j = y;
-	i = x;
-	ft_putchar('A');
-	if ( i > 1)
-		while ((x - 2 ) > 0)
-			{
-				ft_putchar('B');
-				x--;
-			}
-	x = i;
-	if ( i > 1 )
-		ft_putchar('C');
-	ft_putchar('\n');
+void	ft_putb2(int x, int y)
+{
 	while ((y - 2) > 0)
 	{
 		ft_putchar('B');
-		while ((x - 2) > 0)
-		{
-			ft_putchar(' ');
-			x--;
-		}
-		x = i;
-		if ( i > 1)
-		{
+		ft_putb(x, ' ');
+		if (x > 1)
 			ft_putchar('B');
-		}
 		ft_putchar('\n');
 		y--;
 	}
-	y = j;
-	if ( y > 1 )
+}
+
+void	colle(int x, int y)
+{
+	if (x <= 0 || y <= 0)
+		ft_putchar('\n');
+	else
 	{
 		ft_putchar('A');
-		while ((x - 2 ) > 0 )
-		{
-			ft_putchar('B');
-			x--;
-		}
-		if ( i > 1 )
-		{
-			ft_putchar('C');
-		}
+		ft_putb(x, 'B');
+		if (x > 1)
+			if (y > 1 || x > 1)
+				ft_putchar('C');
 		ft_putchar('\n');
+		ft_putb2(x, y);
+		if (x != 1 || y != 1)
+		{
+			if (y > 1)
+			{
+				ft_putchar('A');
+				ft_putb(x, 'B');
+				if (x > 1)
+					ft_putchar('C');
+				ft_putchar('\n');
+			}
+		}
 	}
 }
--- main.c
+++ main.c
@@ -6,22 +6,18 @@
 /*   By: mpaul <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/03 18:18:00 by mpaul             #+#    #+#             */
-/*   Updated: 2016/09/04 05:01:44 by mpaul            ###   ########.fr       */
+/*   Updated: 2016/09/04 17:31:21 by mpaul            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<unistd.h>
+#include <unistd.h>
 
 int		ft_putchar(char c);
 
-void	colle03(int x, int y);
+void	colle(int x, int y);
 
 int		main(void)
 {
-	colle03(5, 3);
-	colle03(5, 1);
-	colle03(1, 1);
-	colle03(1, 5);
-	colle03(4, 4);
+	colle(5, 1);
 	return (0);
 }
